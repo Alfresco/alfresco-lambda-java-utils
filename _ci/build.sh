8aae591b011c97fb036d734e21e3a1ea4550c3a7
aciubotaru <andrei.ciubotaru@ness.com>
2020-08-17 13:29:57 +0300

    test

--- _ci/build.sh
+++ _ci/build.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 #export AWS_ACCESS_KEY_ID=${CREATE_BUCKET_AWS_ACCESS_KEY}
 #export AWS_SECRET_ACCESS_KEY=${CREATE_BUCKET_AWS_SECRET_KEY}
-echo $AWS_ACCESS_KEY_ID
+env
 
 export BUCKET_NAME="travis-lambda-java-utils-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
 
